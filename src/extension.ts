a7c3d5ffd94915283bed64c7a167da74b48e1353
1vnazarov <nazaroffvana.boeing@yandex.ru>
2025-02-21 18:12:32 +0300

    v 1.0

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,109 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    const log = vscode.window.createOutputChannel('pretty-jsdoc');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "pretty-jsdoc" is now active!');
+    let currentSyntaxDecoration: vscode.TextEditorDecorationType;
+    let currentBackgroundDecoration: vscode.TextEditorDecorationType;
+    let isEnabled = false;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('pretty-jsdoc.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from pretty jsdoc!');
-	});
+    function updateDecorations(editor: vscode.TextEditor) {
+        if (!editor || !isEnabled) return;
 
-	context.subscriptions.push(disposable);
+        if (currentSyntaxDecoration) {
+            currentSyntaxDecoration.dispose();
+        }
+        if (currentBackgroundDecoration) {
+            currentBackgroundDecoration.dispose();
+        }
+
+        currentSyntaxDecoration = vscode.window.createTextEditorDecorationType({
+            opacity: '0'
+        });
+
+        currentBackgroundDecoration = vscode.window.createTextEditorDecorationType({
+            backgroundColor: new vscode.ThemeColor('editor.lineHighlightBackground'),
+            isWholeLine: true,
+        });
+
+        const document = editor.document;
+        const syntaxDecorations: vscode.DecorationOptions[] = [];
+        const backgroundDecorations: vscode.DecorationOptions[] = [];
+        
+        let isInJSDoc = false;
+
+        for (let i = 0; i < document.lineCount; i++) {
+            const line = document.lineAt(i);
+            const text = line.text;
+
+            if (text.includes('/**')) {
+                isInJSDoc = true;
+            }
+
+            if (isInJSDoc) {
+                backgroundDecorations.push({
+                    range: line.range
+                });
+
+                const matches = text.match(/\/\*\*|\*\/|\s*\*\s/g);
+                if (matches) {
+                    matches.forEach(match => {
+                        const startPos = text.indexOf(match);
+                        const range = new vscode.Range(
+                            new vscode.Position(i, startPos),
+                            new vscode.Position(i, startPos + match.length)
+                        );
+                        syntaxDecorations.push({
+                            range: range
+                        });
+                    });
+                }
+            }
+
+            if (text.includes('*/')) {
+                isInJSDoc = false;
+            }
+        }
+
+        editor.setDecorations(currentSyntaxDecoration, syntaxDecorations);
+        editor.setDecorations(currentBackgroundDecoration, backgroundDecorations);
+    }
+
+    const changeDocumentSubscription = vscode.workspace.onDidChangeTextDocument(event => {
+        if (event.document === vscode.window.activeTextEditor?.document) {
+            updateDecorations(vscode.window.activeTextEditor);
+        }
+    });
+
+    const changeEditorSubscription = vscode.window.onDidChangeActiveTextEditor(editor => {
+        if (editor) {
+            updateDecorations(editor);
+        }
+    });
+
+    let toggleDisposable = vscode.commands.registerCommand('pretty-jsdoc.toggle', () => {
+        isEnabled = !isEnabled;
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) return;
+        if (isEnabled) {
+            updateDecorations(editor);
+        }
+        else {
+            if (currentSyntaxDecoration) {
+                currentSyntaxDecoration.dispose();
+            }
+            if (currentBackgroundDecoration) {
+                currentBackgroundDecoration.dispose();
+            }
+        }
+    });
+
+    context.subscriptions.push(
+        toggleDisposable,
+        changeDocumentSubscription,
+        changeEditorSubscription
+    );
+    vscode.commands.executeCommand('pretty-jsdoc.toggle');
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
